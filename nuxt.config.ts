5828ec46b244d29131d07dd388b9734f4d9b1a49
yosecc <yosec.cervino@gmail.com>
2024-01-03 18:41:21 -0300

    save;

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,30 +6,30 @@ export default defineNuxtConfig({
     head: {
       // Agrega la referencia al archivo CSS
       link: [
-        { rel: 'stylesheet', href: '/public/assets/css/all-fontawesome.min.css' },
-        { rel: 'stylesheet', href: '/public/assets/css/animate.min.css' },
-        { rel: 'stylesheet', href: '/public/assets/css/magnific-popup.min.css' },
-        { rel: 'stylesheet', href: '/public/assets/css/owl.carousel.min.css' },
-        { rel: 'stylesheet', href: '/public/assets/css/jquery-ui.min.css' },
-        { rel: 'stylesheet', href: '/public/assets/css/jquery.timepicker.min.css' },
-        { rel: 'stylesheet', href: '/public/assets/css/bootstrap.min.css' },
-        { rel: 'stylesheet', href: '/public/assets/css/style.css' }
+        { rel: 'stylesheet', href: '/assets/css/all-fontawesome.min.css' },
+        { rel: 'stylesheet', href: '/assets/css/animate.min.css' },
+        { rel: 'stylesheet', href: '/assets/css/magnific-popup.min.css' },
+        { rel: 'stylesheet', href: '/assets/css/owl.carousel.min.css' },
+        { rel: 'stylesheet', href: '/assets/css/jquery-ui.min.css' },
+        { rel: 'stylesheet', href: '/assets/css/jquery.timepicker.min.css' },
+        { rel: 'stylesheet', href: '/assets/css/bootstrap.min.css' },
+        { rel: 'stylesheet', href: '/assets/css/style.css' }
       ],
       script: [
-        { src: '/public/assets/js/jquery-3.6.0.min.js',  },
-        { src: '/public/assets/js/modernizr.min.js',  },
-        { src: '/public/assets/js/bootstrap.bundle.min.js',  },
-        { src: '/public/assets/js/imagesloaded.pkgd.min.js',  },
-        { src: '/public/assets/js/jquery.magnific-popup.min.js',  },
-        { src: '/public/assets/js/isotope.pkgd.min.js',  },
-        { src: '/public/assets/js/jquery.appear.min.js',  },
-        { src: '/public/assets/js/jquery.easing.min.js',  },
-        { src: '/public/assets/js/owl.carousel.min.js',  },
-        { src: '/public/assets/js/counter-up.js',  },
-        { src: '/public/assets/js/jquery-ui.min.js',  },
-        { src: '/public/assets/js/jquery.timepicker.min.js',  },
-        { src: '/public/assets/js/wow.min.js',  },
-        { src: '/public/assets/js/main.js',  }
+        { src: '/assets/js/jquery-3.6.0.min.js',  },
+        { src: '/assets/js/modernizr.min.js',  },
+        { src: '/assets/js/bootstrap.bundle.min.js',  },
+        { src: '/assets/js/imagesloaded.pkgd.min.js',  },
+        { src: '/assets/js/jquery.magnific-popup.min.js',  },
+        { src: '/assets/js/isotope.pkgd.min.js',  },
+        { src: '/assets/js/jquery.appear.min.js',  },
+        { src: '/assets/js/jquery.easing.min.js',  },
+        { src: '/assets/js/owl.carousel.min.js',  },
+        { src: '/assets/js/counter-up.js',  },
+        { src: '/assets/js/jquery-ui.min.js',  },
+        { src: '/assets/js/jquery.timepicker.min.js',  },
+        { src: '/assets/js/wow.min.js',  },
+        { src: '/assets/js/main.js',  }
       ]
     },
     

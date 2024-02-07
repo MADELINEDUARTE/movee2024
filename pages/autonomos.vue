9dce550f281921aebeba01e1cac68b43c09d0530
MADELINEDUARTE <eni198711@gmail.com>
2024-02-07 17:14:45 +0100

    enero7

---
+++ pages/autonomos.vue
@@ -0,0 +1,134 @@
+<template>
+<LayoutWeb>
+    <!-- breadcrumb -->
+        <div class="site-breadcrumb" style="background: url(assets/img/autonomos/01.png)">
+            <div class="container">
+                <h2 class="breadcrumb-title">Ofertas de Renting <br> <span>para Autónomos</span> </h2>
+                    <br>
+                    <br>
+                    <div>
+                        <a href="ofertas" class="theme-btn">Ver Coches &nbsp;<i class="far fa-arrow-right"></i></a>
+                    </div>
+            </div>
+        </div>
+
+        <div class="about-area">
+            <div class="container">
+                <div class="col-lg-">
+            <div class="about-right">
+                <div class="site-heading mb-3">
+                   
+                    <h2 class="site-title">
+                        Ofertas destacadas <span>para Autónomos </span>
+                    </h2>
+                </div>
+                <p>Todo incluido | Sin entrada
+                    Configura tu nuevo coche a medida
+                    Ofertas para Autónomos</p>
+            </div>
+        </div>
+            </div>
+        </div>
+
+    <div class="car-area bg py-120">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 mx-auto">
+                </div>
+                <div class="row">
+        <div class="col-lg-6 col-xl-4">
+            <div class="car-item">   
+                <div class="car-img">
+                    <img src="/assets/img/car/01.png" alt="">
+                </div>
+                <div class="car-content">
+                    <div class="car-top">
+                        <div>
+                            <h4 class="textflex">Renting Empresas</h4>
+                        </div>
+                    </div>
+                     <div class="car-top">
+                            <h4><a href="#">BMW X1 <br> SDrive 18d</a></h4>
+                            <span><i class="fas fa-star"></i> 5.0</span>
+                        </div>
+                    <div>
+                        <div class="car-top">
+                        <span class="car-price"><sub>desde </sub> <strong> $390 </strong> <sub>Al mes</sub></span>
+                        </div>
+                    </div>
+                    <div class="car-footer">
+                            
+                            <a href="configuratuoferta" class="theme-btn1">Particulares</a>
+                            <a href="configuratuoferta" class="theme-btn1">Empresas</a>
+                            <a href="configuratuoferta" class="theme-btn1">Autónomos</a>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+            </div>
+            <div class="text-center mt-4">
+            <a href="ofertas" class="theme-btn">Ver Destacados </a>
+        </div>
+        </div>
+    </div>
+    
+    <div class="about-area">
+            <div class="container">
+                <div class="col-lg-">
+            <div class="about-right">
+                <div class="site-heading mb-3">
+                   
+                    <h2 class="site-title">
+                        En MOVEE ofrecemos <span>coches por suscripción sin entrada </span>
+                    </h2>
+                </div>
+                <p>La nueva solución de movilidad que está revolucionando el sector de la automoción. ¿Quieres disponer de un vehículo de la marca que siempre has deseado sin gastos iniciales? ¿Necesitas un coche solamente
+                para 1 mes? <br> ¿Y para unas vacaciones?</p>
+            </div>
+        </div>
+            </div>
+        </div>
+
+    <div class="car-area bg py-120">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 mx-auto">
+                </div>
+                <div class="row">
+        <div class="col-lg-6 col-xl-4">
+            <div class="car-item">   
+                <div class="car-img">
+                    <img src="/assets/img/car/01.png" alt="">
+                </div>
+                <div class="car-content">
+                    <div class="car-top">
+                        <div>
+                            <h4 class="textflex">Renting Autónomos</h4>
+                        </div>
+                    </div>
+                     <div class="car-top">
+                            <h4><a href="#">BMW X1 <br> SDrive 18d</a></h4>
+                            <span><i class="fas fa-star"></i> 5.0</span>
+                        </div>
+                    <div>
+                        <div class="car-top">
+                        <span class="car-price"><sub>desde </sub> <strong> $390 </strong> <sub>Al mes</sub></span>
+                        </div>
+                    </div>
+                    <div class="car-footer">
+                            
+                            <a href="configuratuoferta.html" class="theme-btn1">Autónomos</a>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+            </div>
+            <div class="text-center mt-4">
+            <a href="ofertas" class="theme-btn">Ver Coches </a>
+        </div>
+        </div>
+    </div>
+</LayoutWeb>
+</template>
